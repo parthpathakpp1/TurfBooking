acd87079ed388a4ba5fe4623bd1842c70939c370
parthpathakpp1 <85991449+parthpathakpp1@users.noreply.github.com>
2023-10-01 10:28:45 +0530

    changes

--- src/pages/TurfsDetail.jsx
+++ src/pages/TurfsDetail.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
-import { useParams,} from 'react-router-dom';
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { turfInfo } from '../data/PlaygroundsData';
 import DateTimePicker from '../components/Turfs/DateTimePicker';
 import Footer from '../components/Footer/Footer';
+import Navbar from '../components/Navbar'
 
 const TurfDetails = () => {
   const { turfId } = useParams();
@@ -18,10 +19,18 @@ const TurfDetails = () => {
     return <div>Turf not found</div>;
   }
 
+  // Function to handle booking action (You can replace this with your actual booking logic)
+  const handleBookTurf = () => {
+    alert(`Turf booked for ${selectedDate.toDateString()} at ${selectedTime}`);
+  };
 
-  
   return (
     <>
+    <div className="bg-black pb-6">
+         <Navbar />
+    </div>
+ 
+    <div className="bg-gray-200">
       <div className="bg-white shadow-md rounded-lg p-4 mx-auto max-w-md">
         <div className="text-center">
           <h2 className="text-2xl font-semibold">{selectedTurf.turf_name}</h2>
@@ -45,7 +54,16 @@ const TurfDetails = () => {
           />
         </div>
 
-   
+        {/* Book Turf Button */}
+        <div className="mt-6">
+          <button
+            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full"
+            onClick={handleBookTurf}
+          >
+            Book Turf
+          </button>
+        </div>
+      </div>
       </div>
       <Footer />
     </>
